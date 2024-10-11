617b7ea0840e00e44b06a9498374483e4bb953ec
april <apriljohnalolod1@gmail.com>
2024-10-11 14:26:17 +0800

    feat(domain): Validate alarm days of week
    Added validation to ensure that the alarm has at least one day of the week selected. Throws an InvalidAlarmException if the daysOfWeek set is empty.

--- app/src/main/java/re/recodestudios/clock_app/feature_clock/domain/use_case/AddAlarm.kt
+++ app/src/main/java/re/recodestudios/clock_app/feature_clock/domain/use_case/AddAlarm.kt
@@ -1,12 +1,17 @@
 package re.recodestudios.clock_app.feature_clock.domain.use_case
 
 import re.recodestudios.clock_app.feature_clock.domain.model.Alarm
+import re.recodestudios.clock_app.feature_clock.domain.model.InvalidAlarmException
 import re.recodestudios.clock_app.feature_clock.domain.respository.AlarmRepository
 
 class AddAlarm(
     private val repository: AlarmRepository
 ) {
+    @Throws(InvalidAlarmException::class)
     suspend operator fun invoke(alarm: Alarm) {
+        if(alarm.daysOfWeek.isEmpty()) {
+            throw InvalidAlarmException("The alarm must have at least one day of the week.")
+        }
         repository.insertAlarm(alarm)
     }
 }
